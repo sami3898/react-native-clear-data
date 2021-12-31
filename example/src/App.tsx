498a325c9184f64bb2f5f14d30f53b655e687de8
Abdulsami <sami.sk868@gmail.com>
2021-12-31 23:00:45 +0530

    App.tsx
    - Update Example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,42 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-clear-data';
+import { StyleSheet, View, Text, Button, Alert } from 'react-native';
+import { clearAppData } from 'react-native-clear-data';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    
   }, []);
+
+  // Display alert before removing data
+  const alert = ()  => {
+    Alert.alert(
+      'Clear Data',
+      'Clear Data will kill your application\nDo you want to Clear Data?',
+      [
+        {
+          text: 'Yes',
+          onPress: () => clearAppData()
+        },
+        {
+          text: 'No',
+          onPress: () => null
+        },
+      ]
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.title}>Welcome Geek</Text>
+      <Text style={styles.desc}>Simple React Native library which allows to delete app data for android and iOS</Text>
+      <Text style={styles.note}>Note: Removing data will kill your app current state. Use it with some prompt so user can not get worry about it</Text>
+      <Button 
+        title='Clear Data'
+        onPress={() => alert()}
+      />
     </View>
   );
 }
@@ -28,4 +52,22 @@ const styles = StyleSheet.create({
     height: 60,
     marginVertical: 20,
   },
+  title: {
+    fontSize: 20,
+    textAlign: 'left',
+    fontWeight: 'bold'
+  },
+  desc: {
+    fontSize: 18,
+    marginTop: 20,
+    marginHorizontal: 20,
+    textAlign: 'left'
+  },
+  note: {
+    fontSize: 12,
+    marginVertical: 20,
+    marginHorizontal: 20,
+    fontWeight: 'bold',
+    color: 'red'
+  }
 });
